1b42de4f9871d5b59f30bc319cac38050aa6e58b
Hyogeol Lee <hyogeollee@gmail.com>
2022-12-09 09:32:08 +0900

    Update example text

--- EmbeddableCommonLispNetSandbox/Program.cs
+++ EmbeddableCommonLispNetSandbox/Program.cs
@@ -9,6 +9,43 @@
     public class Program
     {
         /// <summary>
+        /// Run loop
+        /// </summary>
+        static void Run()
+        {
+            var engine = new EclEngine();
+            engine.RegisterFunction("foo", () => new EclObject(42));
+            engine.RegisterFunction("bar", (x, y) => new EclObject(x.FixNum + y.FixNum));
+
+            var exit = engine.Read(":EXIT");
+            var result = EclObject.Nil;
+
+            Console.WriteLine(";; Enter :EXIT to break loop");
+            Console.WriteLine(";; Embedded functions");
+            Console.WriteLine(";;  - foo : returns 42. e.g. (foo)");
+            Console.WriteLine(";;  - bar : add two number. e.g. (bar 1 2)");
+
+            while (!result.IsEqual(exit))
+            {
+                Console.WriteLine();
+                Console.Write("> ");
+
+                try
+                {
+                    var form = engine.Call("(read)");
+                    result = engine.Eval(form);
+                    engine.Print(result);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Error on eval: " + ex);
+
+                    result = EclObject.Nil;
+                }
+            }
+        }
+
+        /// <summary>
         /// Main entry point
         /// </summary>
         /// <param name="args">Program arguments.</param>
@@ -18,22 +55,7 @@
 
             try
             {
-                var engine = new EclEngine();
-                engine.RegisterFunction("foo", () => new EclObject(42));
-                engine.RegisterFunction("bar", (x, y) => new EclObject(x.FixNum + y.FixNum));
-
-                var exit = engine.Read(":EXIT");
-                var result = EclObject.Nil;
-
-                while (!result.IsEqual(exit))
-                {
-                    Console.WriteLine();
-                    Console.Write("> ");
-
-                    var form = engine.Call("(read)");
-                    result = engine.Eval(form);
-                    engine.Print(result);
-                }
+                Run();
             }
             catch (Exception ex)
             {
